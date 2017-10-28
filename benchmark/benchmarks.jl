68cebe1f95d50fe8e997233c7adefccccc456f9c
Christian Kurz <c.kurz@tu-bs.de>
2017-10-28 23:25:00 +0200

    added working benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -4,6 +4,7 @@ using PkgBenchmark
 @benchgroup "Benchmarks" begin
     n = 7000
     generatedpoints = [collect(1:n) ones(n)]
+    @bench "findminnorm" findminnorm(generatedpoints, [n-1 42]) 
     @bench "fnp" fnp(generatedpoints, [n-1 42]) 
 end
 
