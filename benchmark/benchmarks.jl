ea8523e96ac9bd32470a3cb63ea223b1562add08
Toni Lubiniecki <lubiniecki@iffhz.ing.tu-bs.de>
2017-10-29 17:28:18 +0100

    pointsvecnorm benchmark added

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,10 +1,26 @@
 using Bamberg
 using PkgBenchmark
+using Compat
 
-@benchgroup "Benchmarks" begin
-    n = 7000
-    generatedpoints = [collect(1:n) ones(n)]
-    @bench "findminnorm" findminnorm(generatedpoints, [n-1 42]) 
-    @bench "fnp" fnp(generatedpoints, [n-1 42]) 
+n = 1e7
+
+#   Abfang
+if n < 2 
+    n = 2
 end
 
+@benchgroup "Benchmarks FlatArray" begin
+    gridpoints_fa = [collect(1:n) ones(n)]
+
+    @bench "findminnorm" findminnorm(gridpoints_fa, [n-1 1]) 
+    @bench "fnp" fnp(gridpoints_fa, [n-1 1]) 
+end
+
+@benchgroup "Benchmarks NestedArray" begin
+    gridpoints_na = [[i 1] for i in 1:n]
+
+    @bench "pointsvecnorm" pointsvecnorm(gridpoints_na, [n-1 1])
+end
+
+# res = benchmarkpkg("Bamberg")
+# showall(res)
