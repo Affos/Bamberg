dfa70639ce2ca1f4408ff70b3ba8af16903b0614
Christian Kurz <c.kurz@tu-bs.de>
2017-10-29 16:49:58 +0100

    nested array version

--- src/pointdistance.jl
+++ src/pointdistance.jl
@@ -15,13 +15,10 @@ function fnp(list, ego)
     list[:,1] = list[:,1].^2
     list[:,2] = list[:,2].^2
     distance = list[:,1]+list[:,2]
-    value, idx = findmin(distance)
-    return idx 
+    findmin(distance)
 end
 
-function fmn(list, ego)
-    list =  .- ego
-    res = mapslices(x -> sum(abs2,x), list, 2)  
-    _, idx = findmin(res) 
-    return idx
+function pointsvecnorm(list, ego)
+    list .-+ ego
+    findmin(norm.(list))
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,9 @@ using Base.Test
 
 for n in 1000:1000:10000
     generatedpoints = [collect(1:n) ones(n)]
+    pointsvector = [rand(2) for i in 1:n]
     @testset "Number of points: $n" begin
         @test fnp(generatedpoints, [n-1 42]) == n-1
+        @test pointsvecnorm(pointsvector,  [n-1 42])[2] == n-1
     end
 end
