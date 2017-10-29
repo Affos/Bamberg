00395bec6a3314a301e75fbc686b42fc642181d0
Christian Kurz <c.kurz@tu-bs.de>
2017-10-29 23:51:00 +0100

    faster functions, allow travis nightly fails

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,15 @@
 using Bamberg
-using Base.Test
+using Base.Test, StaticArrays
 
 for n in 1000:1000:10000
     gridpoints_fa = generateidealdata(n,"flat")    
     gridpoints_na = generateidealdata(n,"nested")
+    gridpoints_st = generateidealdata(n,"static-nested")
     @testset "Number of points: $n" begin
         @test findminnorm(gridpoints_fa, [n-1 1]')[2]  == n-1
         @test fnp(gridpoints_fa, [n-1 1]')[2]  == n-1
         @test pointsvecnorm(gridpoints_na, [n-1 1]')[2] == n-1
+        @test pointsvecnorm(gridpoints_st, SVector{2, Int}(n-1, 1))[2] == n-1
+        @test pointsvecnorm_opt(gridpoints_st, SVector{2, Int}(n-1, 1))[2] == n-1
     end
 end
